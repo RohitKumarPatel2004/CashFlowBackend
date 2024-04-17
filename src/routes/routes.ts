0a85a38b231419a520a6ccfa89631599dbf2f45f
pandovinay <vinay.kumar@pandoconsultants.com>
2024-04-17 15:34:25 +0530

    signup_api

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { UserRoutes } from './User/UserRoute';
+import { AuthRoutes } from './Auth/AuthRoute';
 
 const router = express.Router();
 
 router.use('/user', UserRoutes);
+router.use('/auth', AuthRoutes);
 
 export default router;
