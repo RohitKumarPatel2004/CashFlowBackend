de7bd74475edaf69cbef0fbc9bbc5a4938d4783d
pandovinay <vinay.kumar@pandoconsultants.com>
2024-04-18 12:14:30 +0530

    Update: validation Update

---
+++ src/Services/PostSignupAuthDataService/PostSignupAuthDataService.ts
@@ -0,0 +1,28 @@
+import {execute} from "../../Config/Database/QueryWrapperMysql"
+import {ErrorWhileRegisterPanelistException} from '../../Exceptions/ErrorWhileRegisterPanelist'
+import { Request, Response } from "express";
+export const PostSignupAuthDataService = {
+    PostSignupAuthData: async ( request: Request, response: Response) => {
+      try {
+        const { username , email , password } = request.body;
+
+        // if(!username || !email || !password){
+          // throw new  ErrorWhileRegisterPanelistException
+        // }
+
+      if (typeof(username)!=="string"|| typeof(email)!=="string"|| typeof(password)!=="string" ||!username || !email || !password) {
+        throw new ErrorWhileRegisterPanelistException
+      }
+      
+        
+       const createUserQuery = 'INSERT INTO users_detail (username, email, password) VALUES (?, ?, ?)';
+      await execute(createUserQuery, [username, email, password])
+      
+      response.status(200).json({ message: 'Signup successful' });
+    } catch (error: any) {
+      console.error('Error Signup data:', error.message);
+      throw error;
+    }
+  },
+};
+
