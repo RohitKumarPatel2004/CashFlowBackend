6df5499f68e0ab9c13f8294048b0e82a3dca89ae
VINAY KUMAR <166704017+pandovinay@users.noreply.github.com>
2024-04-23 12:00:16 +0530

    Signup api hashed password (#13)
    * signup_api_update_with_token
    
    signup_api_update_with_token
    
    * Signup_API_hashedPassword

--- src/Services/PostSignupAuthDataService/PostSignupAuthDataService.ts
+++ src/Services/PostSignupAuthDataService/PostSignupAuthDataService.ts
@@ -1,23 +1,24 @@
+import { PanelistAlreadyExistException } from "../../Exceptions/PanelistAlreadyExist";
 import {execute} from "../../Config/Database/QueryWrapperMysql"
 import {ErrorWhileRegisterPanelistException} from '../../Exceptions/ErrorWhileRegisterPanelist'
 import { Request, Response } from "express";
+import bcrypt from 'bcrypt';
 export const PostSignupAuthDataService = {
     PostSignupAuthData: async ( request: Request, response: Response) => {
       try {
         const { username , email , password } = request.body;
-
-
-        
-
       if (typeof(username)!=="string"|| typeof(email)!=="string"|| typeof(password)!=="string" ||!username || !email || !password) {
         throw new ErrorWhileRegisterPanelistException
+      } 
+      const userExistsQuery = 'SELECT * FROM users_detail WHERE  email = ?';
+      const existingUsers:any = await execute(userExistsQuery, [username, email]);
+      if (existingUsers.length>0) {
+        throw new PanelistAlreadyExistException
       }
-      
-        
+      const hashedPassword = await bcrypt.hash(password, 10);
        const createUserQuery = 'INSERT INTO users_detail (full_name, email, password) VALUES (?, ?, ?)';
-      await execute(createUserQuery, [username, email, password])
-      
-      response.status(200).json({ message: 'Signup successful' });
+      await execute(createUserQuery, [username, email, hashedPassword])
+      response.status(200).json({ message: 'Signup successful'});
     } catch (error: any) {
       console.error('Error Signup data:', error.message);
       throw error;
