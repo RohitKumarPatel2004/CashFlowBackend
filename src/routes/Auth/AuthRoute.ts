87991f91049843aba1b4462a0fd27ec4fa122942
RohitKumarPatel2004 <rohit.kumar@pandoconsultants.com>
2024-05-08 10:29:21 +0530

    add_signupData_Validation
    using  yup  library

--- src/routes/Auth/AuthRoute.ts
+++ src/routes/Auth/AuthRoute.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { SignupAuthDataController } from '../../Controllers/SignupAuthDataController/SignupAuthDataController';
 import {GetSigninDataServices} from '../../Controllers/GetLoginDataController/GetLoginDataController';
+import { validate } from '../../Middleware/ValidateSignupData/ValidateData';
+import { userSchema } from '../../Middleware/ValidateSignupData/Schema/userSchema';
 const router = express.Router();
 
 router.post('/login', GetSigninDataServices.GetPanelistData)
-router.post('/signupAuthData', SignupAuthDataController.PostSignupAuthData);
+router.post('/signupAuthData',validate(userSchema) ,SignupAuthDataController.PostSignupAuthData);
 
 
 export { router as AuthRoutes };
